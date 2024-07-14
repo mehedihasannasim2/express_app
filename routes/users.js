790f676ddfb50d91306aa6e51b1d8d931634322c
nasim-etlimited <nasim@etlimited.net>
2024-07-14 13:17:25 +0600

    hashing password

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const {User, validate} = require('../models/user');
 const mongoose = require('mongoose');
@@ -13,6 +14,9 @@ router.post('/', async (req, res) => {
 
     user = new User(_.pick(req.body, ['name', 'email', 'password'])); 
 
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+
     await user.save();
 
     res.send(_.pick(user, ['_id','name', 'email']));
@@ -20,53 +24,5 @@ router.post('/', async (req, res) => {
 });
 
 
-
-// router.post('/', async (req, res) => { 
-//     const users = await User.find().sort('name');
-//     res.send(users);
-// })
-
 module.exports = router; 
 
-// router.post('/', async (req, res) => {
-//     const { error } = validateuser(req.body);
-//     if (error) return res.status(400).send(error.details[0].message);
-
-//     let user = new user({ name: req.body.name });
-//     user = await user.save();
-
-//     res.send(user);
-// });
-
-
-// router.put('/:id', async (req, res) =>{
-//     const { error } = validateuser(req.body); 
-//     if (error) return res.status(400).send(result.error.details[0].message);
-
-//     const user = await user.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
-//         new: true 
-//     });
-
-//     if (!user) return res.status(404).send('This user with the given ID was not found.');
-
-//     res.send(user)
-
-// });
-
-
-// router.delete('/:id', async (req, res) => {
-//     const user = await user.findOneAndDelete({ _id: req.params.id });
-    
-//     if (!user) return res.status(404).send('This user with the given ID was not found.');
-    
-//     res.send(user);
-// });
-
-
-// router.get('/:id', async (req, res) => {
-//     const user = await user.findById(req.params.id)
-
-//     if (!user) return res.status(404).send('This user with the given ID was not found.');
-    
-//     res.send(user);
-// });
