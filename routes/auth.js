ddb9e61094caf1fce41d652e4a23b476eced68db
nasim-etlimited <nasim@etlimited.net>
2024-07-15 17:30:34 +0600

    Encapsulating Logic in Mongoose Models

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,3 @@
-const config = require('config');
-const jwt = require('jsonwebtoken');
 const Joi = require('joi');
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
@@ -19,7 +17,10 @@ router.post('/', async (req, res) => {
     const validPassword  = await bcrypt.compare(req.body.password, user.password);
     if (!validPassword) return res.status(400).send('Invalid email or password..');
 
-    const token = jwt.sign({ _id: user._id }, config.get('jwtPrivateKey'));
+
+    const token = user.generateAuthToken();
+
+    // const token = jwt.sign({ _id: user._id }, config.get('jwtPrivateKey'));
     res.send(token); 
 });
 
