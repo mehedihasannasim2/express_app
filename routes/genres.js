aa0b6960508318bcd06fb7d3a21dd068ccf9beb0
nasim-etlimited <nasim@etlimited.net>
2024-07-27 15:08:45 +0600

    Express Error Middleware

--- routes/genres.js
+++ routes/genres.js
@@ -6,13 +6,13 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/', async (req, res) => { 
+router.get('/', async (req, res, next) => { 
     try {
         const genres = await Genre.find().sort('name');
         res.send(genres);
     }
     catch (ex) {
-        res.status(500).send('Something failed.');
+        next(ex);
     }
 });
 
